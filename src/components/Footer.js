f3d6343a8a61a2444bcafa730e018fd770d6917a
Miriam Gurrieri <mion93@hotmail.it>
2021-08-28 11:33:48 +0000

    footer edits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,11 +8,9 @@ function Footer() {
     <div className='footer-container'>
       <section className='footer-subscription'>
         <p className='footer-subscription-heading'>
-          Join the Koffi newsletter to get the lastest news and blog articles about coffee!
+          Join our newsletter to get the lastest news about coffee!
         </p>
-        <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
-        </p>
+      
         <div className='input-areas'>
           <form>
             <input
@@ -51,7 +49,7 @@ function Footer() {
           <div class='footer-link-items'>
             <h2>Social Media</h2>
             <Link to='/'>Instagram</Link>
-            <Link to='/'>Facebook</Link>
+            <Link to='/'></Link>
             
           </div>
         </div>
